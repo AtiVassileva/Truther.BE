4b20ae7cd1625aecfa640916f1603e0aed12cefd
Ati <ativassileva@gmail.com>
2023-12-17 17:36:51 +0200

    Scaffolded db context and models

---
+++ Truther.API/Truther.API/Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Truther.API.Models
+{
+    public partial class Comment
+    {
+        public int Id { get; set; }
+        public string Content { get; set; } = null!;
+        public int? UserId { get; set; }
+        public int? PostId { get; set; }
+
+        public virtual Post? Post { get; set; }
+        public virtual User? User { get; set; }
+    }
+}
---
+++ Truther.API/Truther.API/Models/Post.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Truther.API.Models
+{
+    public partial class Post
+    {
+        public Post()
+        {
+            Comments = new HashSet<Comment>();
+            Likes = new HashSet<Like>();
+            Shares = new HashSet<Share>();
+        }
+
+        public int Id { get; set; }
+        public int? UserId { get; set; }
+        public string Title { get; set; } = null!;
+        public string Content { get; set; } = null!;
+
+        public virtual User? User { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+        public virtual ICollection<Like> Likes { get; set; }
+        public virtual ICollection<Share> Shares { get; set; }
+    }
+}
