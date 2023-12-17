1eb3f218252faa54441e32f09f3dbd25cde56d1d
Ati <ativassileva@gmail.com>
2023-12-17 18:17:14 +0200

    Refactored accounts controller

--- Truther.API/Truther.API/Controllers/AccountsController.cs
+++ Truther.API/Truther.API/Controllers/AccountsController.cs
@@ -2,12 +2,15 @@
 
 namespace Truther.API.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     // This controller is responsible for registrations and login operations.
-    public class AccountsController : Controller
+    public class AccountsController : ControllerBase
     {
-        public IActionResult Index()
+        [HttpGet]
+        public IActionResult GetAll()
         {
-            return View();
+            return BadRequest("Not implemented yet.");
         }
     }
 }
