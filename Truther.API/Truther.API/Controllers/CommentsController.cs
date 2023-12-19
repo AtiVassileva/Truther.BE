4aecc3e141b3e37300320c8314912ae1817ee6fb
Ati <ativassileva@gmail.com>
2023-12-19 15:30:08 +0200

    Implemented get comments by post functionality

---
+++ Truther.API/Truther.API/Controllers/CommentsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Truther.API.Models;
+
+namespace Truther.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        private readonly TrutherContext _dbContext;
+
+        public CommentsController(TrutherContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("{postId}/Comments")]
+        public async Task<IActionResult> GetCommentsByPost(int postId)
+        {
+            var comments = await _dbContext.Comments
+                .Where(c => c.PostId == postId)
+                .ToListAsync();
+            return Ok(comments);
+        }
+    }
+}
