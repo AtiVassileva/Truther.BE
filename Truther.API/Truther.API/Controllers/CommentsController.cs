2e83fc85b19a5bea148784f7a37cb942effa9cd1
Ati <ativassileva@gmail.com>
2023-12-20 11:25:51 +0200

    Created user extensions helper class

--- Truther.API/Truther.API/Controllers/CommentsController.cs
+++ Truther.API/Truther.API/Controllers/CommentsController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Truther.API.Infrastructure;
 using Truther.API.Models;
+using Truther.API.Models.RequestModels;
+using static Truther.API.Common.ValidationConstants;
 
 namespace Truther.API.Controllers
 {
@@ -9,19 +12,33 @@ namespace Truther.API.Controllers
     public class CommentsController : ControllerBase
     {
         private readonly TrutherContext _dbContext;
+        private readonly UserExtensions _userExtensions;
 
-        public CommentsController(TrutherContext dbContext)
+        public CommentsController(TrutherContext dbContext, UserExtensions userExtensions)
         {
             _dbContext = dbContext;
+            _userExtensions = userExtensions;
         }
 
-        [HttpGet("{postId}/Comments")]
+        [HttpGet("{postId}")]
         public async Task<IActionResult> GetCommentsByPost(int postId)
         {
             var comments = await _dbContext.Comments
                 .Where(c => c.PostId == postId)
                 .ToListAsync();
             return Ok(comments);
+        }
+
+        [HttpPost("{postId}")]
+        public async Task<IActionResult> Post(int postId, CommentCreateModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ContentLengthErrorMessage);
+            }
+
+
+            return Ok();
         }
     }
 }
---
+++ Truther.API/Truther.API/Models/RequestModels/CommentCreateModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Truther.API.Common.ValidationConstants;
+
+namespace Truther.API.Models.RequestModels
+{
+    public class CommentCreateModel
+    {
+        [Required]
+        [StringLength(ContentMaxLength, MinimumLength = DefaultMinLength, ErrorMessage = ContentLengthErrorMessage)]
+        public string Content { get; set; } = null!;
+    }
+}
