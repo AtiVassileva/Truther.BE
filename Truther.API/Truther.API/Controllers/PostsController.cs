952d714fa47902c97857201dfb0798109c47504a
Ati <ativassileva@gmail.com>
2023-12-21 15:55:17 +0200

    Renamed Home controller to Posts controller

--- Truther.API/Truther.API/Controllers/PostsController.cs
+++ Truther.API/Truther.API/Controllers/PostsController.cs
@@ -1,44 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Routing;
-using Microsoft.Extensions.Configuration;
 using Truther.API.Implementation;
 using Truther.API.Models;
 
 namespace Truther.API.Controllers
 {
     //This controllers is responsible for all operations regarding Truther post - for example: Create a post, Like a post, Comment on a post, Delete a post etc.
-    public class HomeController : Controller
+    public class PostsController : Controller
     {
         private readonly SqlHelper _sqlHelper;
 
-        public HomeController(IConfiguration configuration)
+        public PostsController(IConfiguration configuration)
         {
             _sqlHelper = new SqlHelper(configuration.GetConnectionString("DefaultConnection"));
         }
 
         [HttpGet]
-        [Route("/")]
         public ActionResult<Post[]> GetPosts()
         {
             return _sqlHelper.GetPosts().ToArray();
         }
 
-        [HttpGet]
-        [Route("{postId}")]
+        [HttpGet("{postId:guid}")]
         public ActionResult<Post> GetPost(Guid postId)
         {
             return _sqlHelper.GetPost(postId);
         }
 
         [HttpPost]
-        [Route("post")]
         public Task Create([FromBody] Post post)
         {
             return _sqlHelper.CreatePost(post);
         }
 
-        [HttpPatch]
-        [Route("like/{postId}")]
+        [HttpPatch("like/{postId:guid}")]
         [ValidateAntiForgeryToken]
         public Task LikePost([FromRoute] Guid postId, IFormCollection collection)
         {
