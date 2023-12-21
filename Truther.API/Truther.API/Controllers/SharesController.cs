291eb7c3bf7c0683cbf5a08965957a1ebc8c8aa7
Ati <ativassileva@gmail.com>
2023-12-21 10:22:15 +0200

    Implemented get shares by user functionality

---
+++ Truther.API/Truther.API/Controllers/SharesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Truther.API.Models;
+
+namespace Truther.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SharesController : ControllerBase
+    {
+        private readonly TrutherContext _dbContext;
+
+        public SharesController(TrutherContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<IActionResult> GetSharesByUser(int userId)
+        {
+            var shares = await _dbContext.Shares
+                .Where(sh => sh.UserId == userId)
+                .ToListAsync();
+            return Ok(shares);
+        }
+    }
+}
